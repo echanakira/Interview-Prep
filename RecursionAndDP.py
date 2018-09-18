2517d6a45ee141f6450599ec32ddd5dcc78c934a
Elijah Chanakira <elijahchan21@gmail.com>
2018-09-17 23:55:11 -0400

    Completed 3 Chapter 8 Problems

--- RecursionAndDP.py
+++ RecursionAndDP.py
@@ -17,3 +17,34 @@ def triple_helper(steps, memo):
     else:
         memo[steps] = triple_helper(steps - 1, memo) + triple_helper(steps - 2, memo) + triple_helper(steps - 3, memo)
         return memo[steps]
+
+'''
+Find the index in an Arr[0...n-1] where Arr[i]=i
+'''
+def magic_index(arr, left, right):
+    left = 0
+    right  = len(arr)-1
+    mid = int((left + right)/2)
+    while(left < right):
+        if(arr[mid] > mid): #Search left
+            right = mid
+            mid = int((left + right)/2)
+        elif(arr[mid] < mid):
+            left = mid+1
+            mid = int((left + right)/2)
+        else:
+            return mid
+    return -1
+
+print(magic_index([-5,-3,1,4,6], 0, 5))
+
+
+'''
+    Implement multiply without multiplyinh
+'''
+def recursive_multiply(num1, num2):
+    if(num1 == 0):
+        return 0
+    elif(num2 == 0):
+        return 0
+    return num1 + recursive_multiply(num1, num2-1)
